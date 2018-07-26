073ef385554a08c193383facc1072a8b4a590224
raduz <raduz18@gmail.com>
2018-07-26 16:34:32 +0300

    test sqlite in bottle

--- DataloggerWebApp/WebApi/v1/web_api_service.py
+++ DataloggerWebApp/WebApi/v1/web_api_service.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 from bottle import route, template
 
+@route('/show/:item')
+def show(item, db):
+    row = db.execute('SELECT * from AppVersion where AppVersion=?', item).fetchone()
+    if row:
+        return template('showitem', page=row)
+    return HTTPError(404, "Page not found")
+
+
 @route('/hello/<name>')
 def index(name):
         return template('<b>Hello {{name}}</b>', name=name)
